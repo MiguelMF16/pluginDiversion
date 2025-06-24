d62cbf98c990fe245fc1bf090adf0a30352a1f85
Miguel Ángel <martinfopiani@gmail.com>
2025-06-24 16:04:53 +0200

    Corrige import de comando

--- src/main/java/com/comugamers/spigot/command/CambiarTiempoAnuncioCommand.java
+++ src/main/java/com/comugamers/spigot/command/CambiarTiempoAnuncioCommand.java
@@ -8,7 +8,6 @@ import com.comugamers.spigot.service.anuncio.IAnuncioService;
 import dev.triumphteam.cmd.core.BaseCommand;
 import dev.triumphteam.cmd.core.annotation.Command;
 import dev.triumphteam.cmd.core.annotation.Default;
-import dev.triumphteam.cmd.core.annotation.Arg;
 
 @Command("cambiartiempoanuncio")
 public class CambiarTiempoAnuncioCommand extends BaseCommand {
@@ -23,7 +22,7 @@ public class CambiarTiempoAnuncioCommand extends BaseCommand {
      * @param minutos intervalo en minutos
      */
     @Default
-    public void ejecutar(CommandSender sender, @Arg("minutos") int minutos) {
+    public void ejecutar(CommandSender sender, int minutos) {
         this.anuncioService.cambiarIntervalo(minutos);
         sender.sendMessage("Intervalo de anuncios actualizado a " + minutos + " minutos.");
     }
